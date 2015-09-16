12d826cc7351e862e2b4c49968a45f494febd284
Simon Fan <simon.fan@metricwire.com>
2015-09-16 05:28:43 -0400

    reorganizing files

--- app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("myApp", []);
-
-app.controller('MainController', ['$scope', '$http', function($scope, $http) {
-    $scope.hi = 'hi';
-    $http.get('http://localhost:4000').then(function(res) {
-        console.log(res);
-    });
-}]);
