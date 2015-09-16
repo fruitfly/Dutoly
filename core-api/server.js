d498f152d0ea2805756abfcd38454f7a03d29739
Simon Fan <simon.fan@metricwire.com>
2015-09-16 16:54:50 -0400

    bless REST apis

--- core-api/server.js
+++ core-api/server.js
@@ -1,14 +1,32 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    config = require('./config/development.js'),
+    request = require('request');
 
-// Routes
 app.get('/', function(req, res) {
     res.header("Access-Control-Allow-Origin", "http://localhost:8000");
     res.header("Access-Control-Allow-Methods", "GET");
     res.send('Server is running');
 });
 
+app.get('/10k/:ticker/:year', function(req, res) {
+    console.log(req.params);
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+    res.header("Access-Control-Allow-Methods", "GET");
+    var ticker = req.params.ticker,
+        year = req.params.year,
+        apiKey = config.apiKey,
+        options = {
+            url: 'http://sec.kimonolabs.com/companies/' + ticker + '/forms/10-K/ANN/' + year + '?apiKey=' + apiKey
+        };
+
+        request(options, function(err, response, body) {
+            console.log(body);
+            res.send(200, body);
+        });
+});
+
 // Port
 app.listen(4000, function() {
-    console.log('Server running on: 4000');    
+    console.log('Server running on: 4000');
 });
--- dutoly/app.js
+++ dutoly/app.js
@@ -2,7 +2,14 @@ var app = angular.module("myApp", []);
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
     $scope.hi = 'hi';
-    $http.get('http://localhost:4000').then(function(res) {
-        console.log(res);
-    });
+    $scope.ticker;
+    $scope.year;
+    $scope.data;
+
+    $scope.getFinancials = function() {
+        $http.get('http://localhost:4000/10K/' + $scope.ticker + '/' + $scope.year).then(function(res) {
+            console.log($scope.ticker + ' ' + $scope.year);
+            $scope.data = res;
+        });
+    };
 }]);
